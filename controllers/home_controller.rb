8dcf30b20d22df297319ad904d1331d054824f89
macournoyer <macournoyer@gmail.com>
2011-10-25 14:48:32 -0400

    Implement intercepting filter & before filter.

--- controllers/home_controller.rb
+++ controllers/home_controller.rb
@@ -1,9 +1,20 @@
 class HomeController < Controller
+  before_filter :header
+  after_filter  :footer
+  
   def index
     response.write "Hello from home!"
   end
   
   def nice
     response.write "This is nice!"
+  end
+  
+  def header
+    response.write "<h1>My App</h1>"
+  end
+  
+  def footer
+    response.write "<p>&copy; me</p>"
   end
 end
---
+++ filters.rb
@@ -0,0 +1,21 @@
+require "active_support/all"
+
+module Filters
+  def self.included(base)
+    # base == Controller
+    base.extend ClassMethods
+    base.class_attribute :before_filters
+    base.before_filters = []
+  end
+  
+  module ClassMethods
+    def before_filter(method)
+      self.before_filters += [method]
+    end
+  end
+  
+  def filter
+    before_filters.each { |method| send(method) }
+    yield
+  end
+end
