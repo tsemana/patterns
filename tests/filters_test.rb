5333f6db36a61c8771b51ea5b622fd5001a753fc
macournoyer <macournoyer@gmail.com>
2011-10-25 15:11:46 -0400

    Implement after & around filters.

--- tests/filters_test.rb
+++ tests/filters_test.rb
@@ -2,8 +2,11 @@ require File.dirname(__FILE__) + '/test_helper'
 require "controller"
 
 class TestController < Controller
+  around_filter :around1
+  around_filter :around2
   before_filter :one
   before_filter :two
+  after_filter :three
   
   def initialize(out)
     @out = out
@@ -16,11 +19,28 @@ class TestController < Controller
   def two
     @out << :two
   end
+  
+  def three
+    @out << :three
+  end
+  
+  def around1
+    @out << 1
+    yield
+    @out << 1
+  end
+  
+  def around2
+    @out << 2
+    yield
+    @out << 2
+  end
 end
 
 class FiltersTest < Test::Unit::TestCase
   def test_store_filters
     assert_equal [:one, :two], TestController.before_filters
+    assert_equal [:three], TestController.after_filters
   end
   
   def test_filter
@@ -28,6 +48,6 @@ class FiltersTest < Test::Unit::TestCase
     TestController.new(out).filter do
       out << :process
     end
-    assert_equal [:one, :two, :process], out
+    assert_equal [1, 2, :one, :two, :process, :three, 2, 1], out
   end
 end
