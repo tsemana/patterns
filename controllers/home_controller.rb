3a6e30c04a27b48e97eb399ffc8d30cfa4e07a35
macournoyer <macournoyer@gmail.com>
2012-01-31 12:45:14 -0500

    Implement before_filter/

--- controllers/home_controller.rb
+++ controllers/home_controller.rb
@@ -1,9 +1,27 @@
 class HomeController < Controller
+  before_filter :header
+  # after_filter :footer
+  # around_filter :layout
+  
   def index
     response.write "Hi from home controller"
   end
   
   def nice
     response.write "This is nice"
+  end
+  
+  def header
+    response.write "<h1>My App</h1>"
+  end
+  
+  def footer
+    response.write "<p>&copy; me</p>"
+  end
+  
+  def layout
+    response.write "<html><body>"
+    yield
+    response.write "</body></html>"
   end
 end
